5a730e6dcb81a14217b2badb8c8ef0c3d46402d3
selelel <janrusselgorembalem4@gmail.com>
2024-01-23 14:48:31 +0800

    [FEAT] Add the closing-component file and render to main-page

---
+++ components/closing/main-page.tsx
@@ -0,0 +1,10 @@
+import { FullWidth } from "@components/commons/common";
+import styled from "@emotion/styled";
+
+const ClosingContainer = styled(FullWidth)``;
+
+const MainPageClosing = () => {
+  return <ClosingContainer>MainPageClosing</ClosingContainer>;
+};
+
+export default MainPageClosing;
