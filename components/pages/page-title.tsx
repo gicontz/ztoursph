5959323a4742da46b544cd90b840e2945e1105f9
selelel <janrusselgorembalem4@gmail.com>
2024-01-29 20:54:39 +0800

    [STYLE] Set text and container to fit-height content

--- components/pages/page-title.tsx
+++ components/pages/page-title.tsx
@@ -3,27 +3,48 @@ import React from "react";
 
 interface Props {
   title: string;
-  bgImage: string;
+  bgImage: string | { src: string };
 }
 
 const TextHeader = styled.h1`
   text-align: center;
-  width: 100%;
+  display: flex;
+  justify-content: center;
+  width: 740px;
   font-family: "Source Serif Pro";
   font-size: 4rem;
-  white-space: nowrap;
+  margin: auto;
 
   @media screen and (max-width: 740px) {
+    width: 85%;
     font-size: 3rem;
   }
 `;
 
-const HeaderContainer = styled.div<{ bgImg: string }>`
-  background-image: url(${({ bgImg }) => bgImg});
-  padding: 3rem;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
+interface HeaderContainerProps {
+  bgImg: string | { src: string };
+}
+
+const HeaderContainer = styled.div<HeaderContainerProps>`
+  position: relative;
+  padding: 1rem 3rem;
+  height: fit-content;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${({ bgImg }) =>
+      typeof bgImg === "string" ? bgImg : bgImg.src});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    filter: brightness(85%);
+    z-index: -1;
+  }
 `;
 
 const PageTitle: React.FC<Props> = ({ bgImage, title }) => {
