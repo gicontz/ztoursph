49a0bca0c7778acad54cfbd577a13dab66f18fea
selelel <janrusselgorembalem4@gmail.com>
2024-02-08 14:32:39 +0800

    [Finalize Fix]

--- modules/tours/types.ts
+++ modules/tours/types.ts
@@ -1,21 +1,24 @@
 export type TTourResponse = {
-    id: number;
-    tour_slug: string;
-    tour_title: string;
-    tour_banner_image: string;
-    package_details: string;
-    price: number;
-    discount: number;
-    gallery: string[];
-  };
-  
-  export type TToursResponse = {
-    tour_slug: string;
-    tour_title: string;
-    package_details: string;
-    price: number;
-    discount: number;
-    tour_banner_image: string;
-    gallery: [];
-  };
-  
+  id: number;
+  tour_slug: string;
+  tour_title: string;
+  tour_banner_image: string;
+  package_details: string;
+  price: number;
+  discount: number;
+  gallery: string[];
+};
+
+export type TToursResponse = {
+  id: number;
+  tour_slug: string;
+  tour_title: string;
+  package_details: string;
+  price: number;
+  discount: number;
+  tour_banner_image: string;
+  location: string;
+  reviews: number;
+  numberReviews: number;
+  gallery: [];
+};
