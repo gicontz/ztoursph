dda2bd55b40be95035d69974949cd57480cb406c
selelel <janrusselgorembalem4@gmail.com>
2024-02-11 12:27:34 +0800

    [FEAT] Create package page

--- pages/packages/index.tsx
+++ pages/packages/index.tsx
@@ -1,13 +1,12 @@
 import styled from "@emotion/styled";
 import HeaderSection from "@components/commons/header-section";
-import ListingCard from "@components/listing/listing-card";
 import { Row } from "@components/commons/common";
 import Layout from "@components/pages/layout";
 import Button from "@components/commons/button";
 import React, { useState } from "react";
-import { TToursResponse } from "@app/modules/tours/types";
 import Loading from "@components/commons/loading";
-import { getTours, useTours } from "@app/modules/tours/actions";
+import { getPackages, usePackages } from "@app/modules/packages/actions";
+import PackageCard from "@components/listing/packages-card";
 
 const ListCardsContainer = styled.div`
   display: flex;
@@ -58,28 +57,32 @@ export default function Tours() {
     pageNumber: 1,
     totalItems: 0,
   });
-  const [store, dispatch] = useTours();
+  const [store, dispatch] = usePackages();
 
   React.useEffect(() => {
     const { pageNumber } = state;
-    console.log('get')
-    getTours(dispatch, { pageNumber, pageSize });
+    console.log("get");
+    getPackages(dispatch, { pageNumber, pageSize });
   }, []);
 
   const handleLoadMore = () => {
     if (store.totalRecords > state.totalItems) {
       const { pageNumber } = state;
-      getTours(dispatch, { pageNumber: pageNumber + 1, pageSize });
-      setState(s => ({ ...s, pageNumber: s.pageNumber + 1, totalItems: s.totalItems + pageSize }));
+      getPackages(dispatch, { pageNumber: pageNumber + 1, pageSize });
+      setState((s) => ({
+        ...s,
+        pageNumber: s.pageNumber + 1,
+        totalItems: s.totalItems + pageSize,
+      }));
     }
-  }
+  };
 
-  console.log(store)
+  console.log(store);
 
   return (
     <Layout contained>
       <Row className="!mt-10">
-        <HeaderSection>Our Tour Collection</HeaderSection>
+        <HeaderSection>Our Package Collection</HeaderSection>
       </Row>
       <Row className="!mt-5 !mb-10">
         <Description>
@@ -92,25 +95,24 @@ export default function Tours() {
       </Row>
 
       <Row>
-        {store?.tours.length !== 0 && store.tours && (
+        {store?.packages.length !== 0 && store.packages && (
           <>
             <ListCardsContainer>
-              {store.tours?.map((data, key) => (
-                <ListingCard key={key} data={data} />
+              {store.packages?.map((data, key) => (
+                <PackageCard key={key} data={data} />
               ))}
             </ListCardsContainer>
           </>
         )}
       </Row>
       <Row className="flex flex-col items-center justify-center space-y-5 !my-10">
-        { store.isLoading && <Loading /> }
-          { (store.tours.length > 0 && store.tours.length !== store.totalRecords) && 
-            <LoadMoreButton
-              onClick={handleLoadMore}
-              type="primary">
+        {store.isLoading && <Loading />}
+        {store.packages.length > 0 &&
+          store.packages.length !== store.totalRecords && (
+            <LoadMoreButton onClick={handleLoadMore} type="primary">
               Load More Tours
             </LoadMoreButton>
-          }
+          )}
       </Row>
     </Layout>
   );
