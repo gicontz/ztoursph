d7b9bf09e8e1673f29b110783eea455f199d3742
selelel <janrusselgorembalem4@gmail.com>
2024-04-24 16:02:30 +0800

    [WIP] Added data privacy and terms-and-condition

---
+++ pages/data-privacy/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/pages/layout";
+import React from "react";
+
+const DataPrivacy = () => {
+  return (
+    <Layout>
+      <p>Data Privacy</p>
+    </Layout>
+  );
+};
+
+export default DataPrivacy;
---
+++ pages/terms-and-condition/index.tsx
@@ -0,0 +1,8 @@
+import Layout from "@components/pages/layout";
+import React from "react";
+
+const TermsAndConditions = () => {
+  return <Layout>TermsAndConditions</Layout>;
+};
+
+export default TermsAndConditions;
