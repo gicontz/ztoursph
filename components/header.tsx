40c48c781cbcddc27353fe6fcab3d3e45402dd85
gicontz <gimelcontz@gmail.com>
2024-05-21 00:44:52 +0800

    update logo

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Flex, Box, Center, Spacer } from "@chakra-ui/react";
 import Image from "next/image";
 import Logo from "@assets/images/logo.png";
+import LogoWhite from "@assets/images/logo-white.png";
 import MainNav from "./navs/main";
 import MobileNavs from "./navs/mobile";
 
@@ -9,7 +10,7 @@ const Header = ({ sticky }) => {
   return (
     <Flex w="100%" gap={4} className={`bg-transparent p-4`}>
       <Box p="2" className="flex items-center">
-        <Image src={Logo} width={150} height={100} alt="logo" />
+        <Image src={sticky ? Logo : LogoWhite} width={150} height={100} alt="logo" />
         <h1 className="hidden">Z Tours PH</h1>
       </Box>
       <Spacer />
